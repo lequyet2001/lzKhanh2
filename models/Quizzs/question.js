c8b8aeaa70c526574319c8084df331397a4e9857
quyet <quyet.lh198327@sis.hust.edu.vn>
2024-12-20 10:38:01 +0700

    Refactor question model to use structured options with isCorrect flag; add difficulty field

--- models/Quizzs/question.js
+++ models/Quizzs/question.js
@@ -3,8 +3,11 @@ const { default: mongoose } = require("mongoose");
 const questionSchema = new mongoose.Schema({
     course_id: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
     question: { type: String, required: true },
-    options: { type: [String], required: true },
-    correctAnswer: { type: Number, required: true },
+    
+    options: { type: [{
+        option: { type: String, required: true },
+        isCorrect: { type: Boolean, required: true }
+    }], required: true },
     difficulty: { type: String, enum: ["easy", "medium", "hard"], required: true },
     createdAt: { type: Date, default: Date.now }
 });
